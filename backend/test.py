e8576a5cd475b32d73678beb197993296225b773
Mehul Chauhan <mehulchauhan1704@gmail.com>
2025-05-21 22:55:34 +0530

    contactpageemaildone1

--- backend/test.py
+++
@@ -1,6 +0,0 @@
-from backend.venv.app import app  # Import the app instance from app.py
-import requests
-
-# Test the /test-smtp route
-response = requests.get("http://127.0.0.1:5000/test-smtp")
-print(response.json())
